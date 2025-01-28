4d69c44d24d0b4638375872cca5d898363442b21
Rhishi-kesh <reshikash300@gmail.com>
2025-01-28 20:37:50 +0600

    Provider Register

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\RepositoryServiceProvider::class,
     Yajra\DataTables\DataTablesServiceProvider::class,
+    App\Providers\RhishiRepositoryServiceProvider::class,
 
 ];
