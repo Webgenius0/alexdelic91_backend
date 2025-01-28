73913f87fb65588cddb95221a037c32d99a42a4e
Rhishi-kesh <reshikash300@gmail.com>
2025-01-28 19:50:08 +0600

    Fix Migration File Code

--- database/migrations/2025_01_28_130949_create_bookings_table.php
+++ database/migrations/2025_01_28_130949_create_bookings_table.php
@@ -12,17 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('bookings', function (Blueprint $table) {
-            Schema::create('bookings', function (Blueprint $table) {
-                $table->id();
-                $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-                $table->foreignId('service_provider_id')->constrained('users', 'id')->onDelete('cascade');
-                $table->time('start_time');
-                $table->time('end_time');
-                $table->date('booking_date');
-                $table->text('notes')->nullable();
-                $table->enum('status', ['pending', 'accepted', 'completed', 'cancelled'])->default('pending');
-                $table->timestamps();
-            });
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('service_provider_id')->constrained('users', 'id')->onDelete('cascade');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->date('booking_date');
+            $table->text('notes')->nullable();
+            $table->enum('status', ['pending', 'accepted', 'completed', 'cancelled'])->default('pending');
+            $table->timestamps();
         });
     }
 
