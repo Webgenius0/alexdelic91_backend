744429ce3329bac5d004cff3a9bc3d56573be017
Rhishi-kesh <reshikash300@gmail.com>
2025-01-28 19:55:32 +0600

    Create API, Backend Route File

--- routes/rhishi_api.php
+++ routes/rhishi_api.php
@@ -20,86 +20,13 @@ use App\Http\Controllers\Api\UserController;
 |
 */
 
-
-//Social Login
-Route::post('/social-login', [SocialAuthController::class, 'socialLogin']);
-
-//Register API
-Route::controller(RegisterController::class)->prefix('users/register')->group(function () {
-    // User Register
-    Route::post('/', 'userRegister');
-
-    // Verify OTP
-    Route::post('/otp-verify', 'otpVerify');
-
-    // Resend OTP
-    Route::post('/otp-resend', 'otpResend');
-});
-
-//Login API
-Route::controller(LoginController::class)->prefix('users/login')->group(function () {
-
-    // User Login
-    Route::post('/', 'userLogin');
-
-    // Verify Email
-    Route::post('/email-verify', 'emailVerify');
-
-    // Resend OTP
-    Route::post('/otp-resend', 'otpResend');
-
-    // Verify OTP
-    Route::post('/otp-verify', 'otpVerify');
-
-    //Reset Password
-    Route::post('/reset-password', 'resetPassword');
-});
-
 Route::group(['middleware' => ['jwt.verify']], function() {
 
     Route::controller(UserController::class)->prefix('users')->group(function () {
         Route::get('/data', 'userData');
-        Route::post('/data/update', 'userUpdate');
-        Route::post('/logout', 'logoutUser');
-        Route::delete('/delete', 'deleteUser');
-
-        Route::post('/service/provider/profile/create','create');
-        Route::get('/days', 'getDays');
-        Route::get('/service/location', 'getLocation');
     });
 
 });
 
-// Category
-Route::group(['middleware' => ['jwt.verify']], function() {
-
-    Route::controller(CategoryController::class)->group(function () {
-        Route::get('/categories', 'categories');
-        Route::get('/sub-categories/{category}','subCategories');
-    });
-
-    Route::controller(HelpCenterController::class)->group(function() {
-        Route::post('/request-for-help','requestForHelp');
-    });
-
-    Route::controller(ProviderController::class)->group(function() {
-        Route::get('/providers','providers');
-        Route::get('/provider-details/{id}','providerDetails');
-    });
-
-});
-
-
-// Help Center
-
-Route::group(['middleware' => ['jwt.verify']], function() {
-
-});
-
-// Provider
-
-Route::group(['middleware' => ['jwt.verify']], function() {
-
-});
 
 
