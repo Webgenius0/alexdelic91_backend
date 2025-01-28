28309806d0083949a977c3d0feee4e80bc8685f2
mizanurmillon <mr7517218@gmail.com>
2025-01-28 20:32:35 +0600

    feedBack Api done

---
+++ database/seeders/FeedbackSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Booking;
+use App\Models\Feedback;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class FeedbackSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+         // Ensure there are users and bookings in the database
+         $bookings = Booking::all();
+
+         foreach ($bookings as $booking) {
+             Feedback::create([
+                 'user_id' => 2,
+                 'booking_id' => $booking->id,
+                 'feedback' => 'This is a sample feedback for booking #' . $booking->id,
+                 'rating' => rand(1, 5),
+             ]);
+         }
+    }
+}
