f42f017742eaeb18db3d21f08b3934c2aa17c90e
Rhishi-kesh <reshikash300@gmail.com>
2025-01-29 11:34:55 +0600

    Fix & Make perfect Booking Create API

--- app/Repositories/BookingProviderRepository.php
+++ app/Repositories/BookingProviderRepository.php
@@ -4,10 +4,14 @@ namespace App\Repositories;
 
 use App\Interface\BookingProviderInterface;
 use App\Models\Booking;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Traits\ApiResponse;
+use Illuminate\Support\Facades\Validator;
 
 class BookingProviderRepository implements BookingProviderInterface
 {
+    use ApiResponse;
     /**
      * Create a new class instance.
      */
@@ -16,23 +20,43 @@ class BookingProviderRepository implements BookingProviderInterface
         //
     }
 
-    public function book(Request $request)
+    public function book($data)
     {
-        $request->validate([
-            'user_id' => 'required|exists:users,id',
-            'start_time' => 'required|date_format:H:i',
-            'end_time' => 'required|date_format:H:i|after:start_time',
-            'booking_date' => 'required|date',
-        ]);
+        // Find the user by ID
+        $user = auth()->user();
+
+        // If user is not found, return an error response
+        if (!$user) {
+            return $this->error([], "User Not Found", 404);
+        }
+
+        $provider = User::with('serviceProviderProfile')->findOrFail($data['service_provider_id']);
+
+        // Check if booking is within the provider's available hours
+        if (!$provider->isAvailableForBooking($data['start_time'], $data['end_time'])) {
+            return $this->error([], "This provider is only available from {$provider->serviceProviderProfile->start_time} to {$provider->serviceProviderProfile->end_time}", 409);
+        }
 
         $booking = new Booking();
 
-        if ($booking->overlaps($request->start_time, $request->end_time, $request->booking_date)) {
-            return response()->json(['message' => 'This time slot is already booked.'], 409);
+        if ($booking->overlaps($data['start_time'], $data['end_time'], $data['booking_date'])) {
+            return $this->error([], "Booking overlaps with another booking", 409);
         }
 
-        Booking::create($request->only(['user_id', 'start_time', 'end_time', 'booking_date']));
+        try {
 
-        return response()->json(['message' => 'Booking successful!'], 201);
+            $booking = Booking::create([
+                'user_id' => $user->id,
+                'service_provider_id' => $data['service_provider_id'],
+                'start_time' => $data['start_time'],
+                'end_time' => $data['end_time'],
+                'booking_date' => $data['booking_date'],
+                'notes' => $data['notes'],
+            ]);
+
+            return $booking;
+        } catch (\Exception $e) {
+            return null;
+        }
     }
 }
