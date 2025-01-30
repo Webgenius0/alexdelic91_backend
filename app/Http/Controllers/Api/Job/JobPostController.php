a7c449a0f4a4fbced610f9b4ffd16229abaa3d0a
mizanurmillon <mr7517218@gmail.com>
2025-01-30 11:30:38 +0600

    job post repository interface and repository class create

---
+++ app/Http/Controllers/Api/Job/JobPostController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Job;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class JobPostController extends Controller
+{
+    //
+}
---
+++ app/Repositories/JobPostRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interface\JobPostRepositoryInterface;
+
+
+class JobPostRepository implements JobPostRepositoryInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+}
