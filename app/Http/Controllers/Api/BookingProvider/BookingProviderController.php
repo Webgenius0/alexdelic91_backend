0f829f3bf58ecc5ebb4887716b6b67ec45f1b8a0
Rhishi-kesh <reshikash300@gmail.com>
2025-01-30 15:16:56 +0600

    Response Issue Findout

--- app/Http/Controllers/Api/BookingProvider/BookingProviderController.php
+++ app/Http/Controllers/Api/BookingProvider/BookingProviderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\BookingProvider;
 
+use App\Exceptions\CustomException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\BookingRequest;
 use App\Interface\BookingProviderInterface;
@@ -24,13 +25,15 @@ class BookingProviderController extends Controller
 
     public function book(BookingRequest $request)
     {
-        $booking = $this->bookingProviderInterface->book($request->validated());
+        try {
+            $booking = $this->bookingProviderInterface->book($request->validated());
 
-        if (!$booking) {
-            return $this->error([], "An error occurred while creating the booking", 500);
+            return $this->success($booking, 'Booking created successfully', 201);
+        } catch (CustomException $e) {
+            return $this->error([], $e->getMessage(), $e->getCode());
+        } catch (\Exception $e) {
+            return $this->error([], "An unexpected error occurred", 500);
         }
-
-        return $this->success($booking, 'Booking created successfully', 201);
     }
 
     public function pastBookings()
