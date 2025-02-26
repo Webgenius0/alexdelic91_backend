71d691041e7535b53b2a147cfe82921fa7d4494f
Rhishi-kesh <reshikash300@gmail.com>
2025-02-26 17:49:40 +0600

    Status Code Change

--- app/Repositories/ReviewsRepository.php
+++ app/Repositories/ReviewsRepository.php
@@ -15,28 +15,29 @@ class ReviewsRepository implements ReviewsInterface
         //
     }
 
-    public function getReviews($query)  {
-       
+    public function getReviews($query)
+    {
+
         switch ($query) {
             case 'only_bad_reviews':
-                $data = Feedback::whereBetween('rating',[1, 3])->with(['user','serviceProvider','booking'])->get();
+                $data = Feedback::whereBetween('rating', [1, 3])->with(['user', 'serviceProvider', 'booking'])->get();
                 break;
             case 'newest_reviews':
-                $data = Feedback::orderBy('created_at','desc')->with(['user','serviceProvider','booking'])->get();
+                $data = Feedback::orderBy('created_at', 'desc')->with(['user', 'serviceProvider', 'booking'])->get();
                 break;
             case 'oldest_reviews':
-                $data = Feedback::orderBy('created_at','asc')->with(['user','serviceProvider','booking'])->get();
+                $data = Feedback::orderBy('created_at', 'asc')->with(['user', 'serviceProvider', 'booking'])->get();
                 break;
             case 'only_good_reviews':
-                $data = Feedback::whereBetween('rating',[4, 5])->with(['user','serviceProvider','booking'])->get();
+                $data = Feedback::whereBetween('rating', [4, 5])->with(['user', 'serviceProvider', 'booking'])->get();
                 break;
             case 'unpopular_reviews':
-                $data = Feedback::where('rating',1)->with(['user','serviceProvider','booking'])->get();
+                $data = Feedback::where('rating', 1)->with(['user', 'serviceProvider', 'booking'])->get();
                 break;
             default:
-                $data = Feedback::with(['user','serviceProvider','booking'])->get();
+                $data = Feedback::with(['user', 'serviceProvider', 'booking'])->get();
                 break;
         }
-       return $data;
+        return $data;
     }
 }
