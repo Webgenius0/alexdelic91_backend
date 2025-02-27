240ac83889f418e506a0ff73c178ea29cd6690b5
Rhishi-kesh <reshikash300@gmail.com>
2025-02-27 11:12:01 +0600

    Single Service provider API Issue Fix

--- app/Http/Controllers/Api/Provider/ProviderController.php
+++ app/Http/Controllers/Api/Provider/ProviderController.php
@@ -18,18 +18,23 @@ class ProviderController extends Controller
         $this->serviceProviderRepository = $serviceProvidersRepository;
     }
 
-    public function providers(Request $request) {
-        $queryParams = $request->only(['business_name','latitude','longitude','radius','category_id','subcategory_id','avg_rating']) ?? [];
+    public function providers(Request $request)
+    {
+        $queryParams = $request->only(['business_name', 'latitude', 'longitude', 'radius', 'category_id', 'subcategory_id', 'avg_rating']) ?? [];
 
         $providers = $this->serviceProviderRepository->providers($queryParams);
 
-        return $this->success($providers,'All providers are here',200);
-
+        return $this->success($providers, 'All providers are here', 200);
     }
 
-    public function providerDetails($id) {
+    public function providerDetails($id)
+    {
         $providerDetails = $this->serviceProviderRepository->getProviderDetails($id);
-        return $this->success($providerDetails,'Provider details are here',200);
 
+        if (!$providerDetails) {
+            return $this->error([], 'Provider not found', 200);
+        }
+
+        return $this->success($providerDetails, 'Provider details are here', 200);
     }
 }
