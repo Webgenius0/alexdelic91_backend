61fed68de01be579d66eaadd023c4a7548177329
mizanurmillon <mr7517218@gmail.com>
2025-03-01 12:21:57 +0600

    dynamic page issuse fix it

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 use App\Models\DynamicPage;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\NotificationController;
+use App\Http\Controllers\Web\Frontend\PageController;
 
 require __DIR__.'/auth.php';
 
