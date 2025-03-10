08eb772b80ab3d05e482b82063839129cd67c652
= <=>
2025-03-10 16:04:15 +0600

    mistake

--- app/Http/Middleware/Cors.php
+++
@@ -1,5 +0,0 @@
-<?php
-return $next($request)
-->header('Access-Control-Allow-Origin', '*')
-->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-->header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
